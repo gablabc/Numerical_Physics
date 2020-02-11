37f92a3dafe41f62e19bf51e8bc83242b13e0a4c
gablabc <gabriel.laberge@polymtl.ca>
2020-02-11 18:39:57 -0500

    Improve 2D puit

--- finite_diff/Puit2D/2D_Puit.py
+++ finite_diff/Puit2D/2D_Puit.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu May 24 00:38:34 2018
-
-@author: gabriel
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
+savefig = False
+if not len(sys.argv) == 1:
+    if sys.argv[1] == 'savefig':
+        savefig = True
+    
 # x**2
 #def f(x):
 #    return x**2
 #def fp(x):
 #    return 2*x
 
-#1-2x**2+x**4
+# 1-2x**2+x**4
 def f(x):
     return 1 - 2 * x**2 + x**4
 def fp(x):
@@ -24,52 +25,57 @@ def fp(x):
 def theta(x):
     return np.arctan(fp(x))
 
-#Physical constants
-g = 1;
-m = 1;
-x = np.linspace(-2, 2, 1000);
+# Physical constants
+g = 1
+m = 1
+x = np.linspace(-2, 2, 1000)
 y = f(x)
-v_norm = 0;
+v_norm = 0
 pos = [-1.7, f(-1.7)]
 v = [0, 0]
 direction = -1 * np.sign(fp(pos[0]))
 E = m * g * f(pos[0])
 
-#Time
-#the maximal time will time_iterations*delta_t
-time_iterations = 1000
+# Time
+# the maximal time will time_iterations*delta_t
+time_iterations = 2000
 delta_t = 0.01
 t_span = np.arange(time_iterations)
 
 
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
-savefig = False
+ax.plot(x, y)
+ax.set_ylim(-0.5, 4)
+lines = [0, 0, 0, 0]
 
+# Time loop for the whole simulation
 for t in t_span:
     if ((t % 10) == 0):
-        ax.clear()
-        #plot current position
-        ax.plot(pos[0], pos[1], "ko")
-        ax.plot(x, y)
-        ax.text(-1, 2, "Energy = "+ str(np.around(1 / 2 * m * v_norm**2 + m * g * pos[1], decimals = 2)))
-        ax.text(-1,2.2,"K = "+ str(np.around(1/2 * m * v_norm**2, decimals = 2)))
-        ax.text(-1,2.4,"U = "+ str(np.around(m * g * pos[1], decimals = 2)))
-        ax.set_ylim(-0.5, 4)
+        # Plot current position
+        lines[0], = ax.plot(pos[0], pos[1], "ko")
+        lines[1] = ax.text(-1, 2, "Energy = "+ str(np.around(1 / 2 * m * v_norm**2 + m * g * pos[1], decimals = 2)))
+        lines[2] = ax.text(-1, 2.2,"K = "+ str(np.around(1/2 * m * v_norm**2, decimals = 2)))
+        lines[3] = ax.text(-1, 2.4,"U = "+ str(np.around(m * g * pos[1], decimals = 2)))
         
         if(savefig):
             fig.savefig("./pictures2D/frame" + str(t) + ".jpg")
         else:
             plt.pause(0.01)
-    
-    #new speed using energy conservation#
-        #particule reaches its apex        
+
+        # Remove the plots except the curve
+        for i in range(4):
+            lines[i].remove()
+
+
+    # New speed using energy conservation#
+    # particule reaches its apex        
     if (2 / m * (E - m * g * f(pos[0])) <= 0):
         direction = -1 * np.sign(fp(pos[0]))
         pos[0] += 0.01 * direction
         pos[1] = f(pos[0])
     else:
-        #calculate new speed
+        # Calculate new speed
         v_norm = np.sqrt(2 / m * (E - m * g * f(pos[0])))
         v = v_norm * direction * np.array([np.cos(theta(pos[0])), np.sin(theta(pos[0]))])
         pos += delta_t*v
--- finite_diff/Puit2D/README.md
+++ finite_diff/Puit2D/README.md
@@ -9,28 +9,34 @@ The simulation is based on the principle of Energy conservation.
  
 This equation give the speed magnitude for a given x position. To obtain the speed orientation, the following equation is used:
 
-<a href="http://www.codecogs.com/eqnedit.php?latex=\theta=atan(f'(x))" target="_blank"><img src="http://latex.codecogs.com/gif.latex?\theta=atan(f'(x))" title="\theta=atan(f'(x))" /></a>
+<a href="http://www.codecogs.com/eqnedit.php?latex=\theta=\text{atan}(f'(x))" target="_blank"><img src="http://latex.codecogs.com/gif.latex?\theta=atan(f'(x))" title="\theta=\atan(f'(x))" /></a>
 
 With these two information about speed, the speed vector can be constructed and used to find the next position using the temporal finite difference equation:
 
 <a href="http://www.codecogs.com/eqnedit.php?latex=\vec{x}_{i&plus;1}=\vec{x}_i&plus;\vec{v}_i&space;\Delta&space;t" target="_blank"><img src="http://latex.codecogs.com/gif.latex?\vec{x}_{i&plus;1}=\vec{x}_i&plus;\vec{v}_i&space;\Delta&space;t" title="\vec{x}_{i+1}=\vec{x}_i+\vec{v}_i \Delta t" /></a>
 
 When the ball reaches the highest point of its position, the kinetic energy becomes null and therefore the algorithm must manually move the
-particle in the oposite direction to restart the movement.
+particle in the opposite direction to restart the movement.
 
 
 # Functions
-Any diffenrentiable function can be used to generate a well so feel free to experiment with your own.
+Any differentiable function can be used to generate a well.
 
 # Save gifs
-To make a gif, you must set the savefig boolean to True. The pictures will be stored in the picture2D repository. Than you can use a software tool to make gifs.
-I use imagemagick
+To make a gif, you must use the savefig parameter when calling the code.
+
+``` 
+python3 2D_Puit.py savefig
+```
+
+The pictures will be stored in the picture2D folder. Than you can use a software tool to make gifs.
+
+I personally use imagemagick
+
 ```
 sudo apt-get install imagemagick
 cd pictures2D
 convert delay -2 -loop 0 `ls -v *.jpg` my_animation.gif 
 ```
 
-Enjoy your animations ;)
-
-![Alt Text](https://github.com/gablabc/Numerical_Physics/blob/master/finite_diff/Puit2D/pictures2D/anim.gif)
+![Alt Text](../../images/finite_diff/animation_puit2D.gif)
